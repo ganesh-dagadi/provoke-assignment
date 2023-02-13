9bd5d161cb619761950c329017088ac361c04f33
Ganesh Dagadi <ganeshdagadi3@gmail.com>
2023-02-13 16:39:20 +0530

    Completed Assignment

--- Controllers/postController.js
+++ Controllers/postController.js
@@ -1,12 +1,84 @@
+const db = require("../Config/firebase");
 const Post = require("../Models/Post")
 module.exports.createPost = async (req , res ,next)=>{
     try{
         const {title , descp} = req.body
         if(!title || !descp) return res.status(400).json({err : "Required parameters are missing"});
-        req.body.userId = req.user.id
         const newPost = new Post(title , descp); // validating data in class constructor
-
+        newPost.userId = req.user.id;
+        const savedPost = await db.collection("posts").add({...newPost})
+        res.status(200).json({msg : "Successfully created a post" , savedPost : await (await savedPost.get()).data()})
     }catch(err){
+        next(err)
+    }
+}
 
+module.exports.getPosts = async (req , res, next)=>{
+    try{
+        const postsSnap = await db.collection("posts").get();
+        const posts = []
+        postsSnap.forEach(post=>{
+            posts.push({...post.data() , id : post.id})
+        })
+        return res.status(200).json({posts})
+    }catch(err){
+        next(err)
+    }
+}
+
+module.exports.getPostById = async (req , res , next)=>{
+    try{
+        const postId = req.params.id
+        const foundPost = await db.collection("posts").doc(postId).get()
+        if(!foundPost.exists) return res.status(404).json({err : "Post not found"})
+        return res.status(200).json({foundPost : foundPost.data()})
+    }catch(err){
+        next(err)
+    }
+}
+
+module.exports.getPostOfUser = async (req , res , next)=>{
+    try{
+        const userId = req.user.id
+        const postsSnap = await db.collection("posts").where("userId" , "==" , userId).get()
+        const posts = []
+        postsSnap.forEach(post=>{
+            posts.push({...post.data() , id : post.id})
+        })
+        return res.status(200).json({posts})
+    }catch(err){
+        next(err)
+    }
+}
+
+module.exports.updatePost = async (req, res , next)=>{
+    try{
+        //ensure logged in user has permission to update
+        const loggedInUser = req.user.id
+        const postSnap = db.collection("posts").doc(req.params.id)
+        const postData = await postSnap.get()
+        if(!postData.exists) return res.status(404).json({err : "Post not found"})
+        if(postData.data().userId != loggedInUser) return res.status(403).json({err : "You are not authorized to perform this operation"})
+
+        //passed
+        await postSnap.update(req.body);
+        res.status(200).json({msg : "Post has been updated"});
+    }catch(err){
+        next(err)
+    }
+}
+
+module.exports.deletePost = async(req,res,next)=>{
+    try{
+        const loggedInUser = req.user.id
+        const postSnap = db.collection("posts").doc(req.params.id)
+        const postData = await postSnap.get()
+        if(!postData.exists) return res.status(404).json({err : "Post not found"})
+        if(postData.data().userId != loggedInUser) return res.status(403).json({err : "You are not authorized to perform this operation"})
+
+        await postSnap.delete()
+        return res.status(200).json({msg : "Post has been deleted"})
+    }catch(err){
+        next(err)
     }
 }
--- Routes/postRoutes.js
+++ Routes/postRoutes.js
@@ -1,9 +1,20 @@
 const express = require("express")
-const { createPost } = require("../Controllers/postController")
+const { createPost , getPosts, getPostById , getPostOfUser , updatePost , deletePost} = require("../Controllers/postController")
 const { authenticateReq } = require("../Middleware/auth")
 
 const router = express.Router()
 
 router.post("/" , authenticateReq , createPost)
+
+//get all posts
+router.get("/" , getPosts)
+
+router.get("/:id" , getPostById)
+
+router.get("/user/all" , authenticateReq , getPostOfUser)
+
+router.put("/:id" , authenticateReq , updatePost)
+
+router.delete("/:id" , authenticateReq , deletePost)
 
 module.exports = router
